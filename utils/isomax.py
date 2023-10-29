7f2c4a2bc02fbb7157e5acb4a56e519e937aa8cc
ailabktw <ailab.ktw@gm.gist.ac.kr>
2023-10-29 16:16:40 +0900

    feat: rearrange isomax code

--- utils/isomax.py
+++ utils/isomax.py
@@ -13,6 +13,7 @@ class IsoMaxPlusLossFirstPart(nn.Module):
         self.temperature = temperature        
         self.prototypes = nn.Parameter(torch.Tensor(num_classes, num_features))
         self.distance_scale = nn.Parameter(torch.Tensor(1)) 
+
         nn.init.normal_(self.prototypes, mean=0.0, std=1.0)
         nn.init.constant_(self.distance_scale, 1.0)
 
@@ -23,6 +24,7 @@ class IsoMaxPlusLossFirstPart(nn.Module):
             F.normalize(features), F.normalize(self.prototypes), p=2.0, compute_mode="donot_use_mm_for_euclid_dist")
         logits = -distances
         # The temperature may be calibrated after training to improve uncertainty estimation.
+
         return logits / self.temperature
 
 
@@ -45,10 +47,12 @@ class IsoMaxPlusLossSecondPart(nn.Module):
         loss = -torch.log(probabilities_at_targets).mean()
         if not debug:
             return loss
+        
         else:
             targets_one_hot = torch.eye(distances.size(1))[targets].long().cuda()
             intra_inter_distances = torch.where(targets_one_hot != 0, distances, torch.Tensor([float('Inf')]).cuda())
             inter_intra_distances = torch.where(targets_one_hot != 0, torch.Tensor([float('Inf')]).cuda(), distances)
             intra_distances = intra_inter_distances[intra_inter_distances != float('Inf')]
             inter_distances = inter_intra_distances[inter_intra_distances != float('Inf')]
+            
             return loss, 1.0, intra_distances, 
