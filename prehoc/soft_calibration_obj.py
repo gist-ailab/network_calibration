1b7cc218b7f39589f21bc2458701f9c2b24ed5a9
ailabktw <ailab.ktw@gm.gist.ac.kr>
2023-10-29 16:37:28 +0900

    feat: rearrange soft_calibration_obj code

--- prehoc/soft_calibration_obj.py
+++ prehoc/soft_calibration_obj.py
@@ -10,13 +10,14 @@ EPS = 1e-6
 def get_soft_avuc_loss(probabilities, labels, soft_avuc_use_deprecated_v0,
                        soft_avuc_temp, soft_avuc_theta):
 
-#   accuracies = tf.dtypes.cast(
-#       tf.math.equal(
-#           tf.argmax(probabilities, axis=1),
-#           tf.cast(labels, tf.int64),
-#       ),
-#       tf.float32,
-#   )
+    # accuracies = tf.dtypes.cast(
+    #     tf.math.equal(
+    #         tf.argmax(probabilities, axis=1),
+    #         tf.cast(labels, tf.int64),
+    #     ),
+    #     tf.float32,
+    # )
+
     accuracies = torch.eq(torch.max(probabilities, dim=1).index, labels).float()
 
     dim = int(probabilities.shape[1])
@@ -24,9 +25,7 @@ def get_soft_avuc_loss(probabilities, labels, soft_avuc_use_deprecated_v0,
     log_safe_probabilities = (1.0 -
                                 EPS) * probabilities + EPS * uniform_probabilities
     log_probabilities = torch.log(log_safe_probabilities)
-    entropies = -(
-        torch.sum(
-            torch.multiply(log_safe_probabilities, log_probabilities), dim=1))
+    entropies = -(torch.sum(torch.multiply(log_safe_probabilities, log_probabilities), dim=1))
 
     entmax = torch.log(dim)
 
@@ -57,28 +56,37 @@ def get_soft_avuc_loss(probabilities, labels, soft_avuc_use_deprecated_v0,
             elems=accuracies_entropies_and_qucs,
             fn=lambda e: tf.convert_to_tensor(e[3]) *
             (tf.constant(1.0) - tf.math.tanh(tf.convert_to_tensor(e[1])))
-            if e[0] > 0.5 else 0.0))
+            if e[0] > 0.5 else 0.0
+        )
+    )
     nau_diff = tf.reduce_sum(
         tf.map_fn(
             elems=accuracies_entropies_and_qucs,
             fn=lambda e: tf.convert_to_tensor(e[2]) * tf.math.tanh(
-                tf.convert_to_tensor(e[1])) if e[0] > 0.5 else 0.0))
+                tf.convert_to_tensor(e[1])) if e[0] > 0.5 else 0.0
+        )
+    )
     nic_diff = tf.reduce_sum(
         tf.map_fn(
             elems=accuracies_entropies_and_qucs,
             fn=lambda e: tf.convert_to_tensor(e[3]) *
             (tf.constant(1.0) - tf.math.tanh(tf.convert_to_tensor(e[1])))
-            if e[0] < 0.5 else 0.0))
+            if e[0] < 0.5 else 0.0
+        )
+    )
     niu_diff = tf.reduce_sum(
         tf.map_fn(
             elems=accuracies_entropies_and_qucs,
             fn=lambda e: tf.convert_to_tensor(e[2]) * tf.math.tanh(
-                tf.convert_to_tensor(e[1])) if e[0] < 0.5 else 0.0))
+                tf.convert_to_tensor(e[1])) if e[0] < 0.5 else 0.0
+        )
+    )
     # pylint: enable=g-long-lambda
 
     avuc_loss = tf.math.log(
         tf.constant(1.0) + (nau_diff + nic_diff) /
-        tf.math.maximum(nac_diff + niu_diff, tf.constant(EPS)))
+        tf.math.maximum(nac_diff + niu_diff, tf.constant(EPS))
+    )
 
     return avuc_loss
 
@@ -114,12 +122,14 @@ class Trainer():
             total_loss += loss
             total += targets.size(0)
             _, predicted = outputs.max(1)            
-            correct += predicted.eq(targets).sum().item()            
+            correct += predicted.eq(targets).sum().item()      
+
             print('\r', batch_idx, len(self.train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                        % (total_loss/(batch_idx+1), 100.*correct/total, correct, total), end = '')    
+                  % (total_loss/(batch_idx+1), 100.*correct/total, correct, total), end = '')    
 
         self.train_accuracy = correct/total
         self.train_avg_loss = total_loss/len(self.train_loader)
+
         print()
 
     def validation(self, epoch):
@@ -134,5 +144,6 @@ class Trainer():
         valid_accuracy = correct/total
         self.scheduler.step()
         self.saver.save_checkpoint(epoch, metric = valid_accuracy)
+        
         print('EPOCH {:4}, TRAIN [loss - {:.4f}, acc - {:.4f}], VALID [acc - {:.4f}]\n'.format(epoch, self.train_avg_loss, self.train_accuracy, valid_accuracy))
         print(self.scheduler.get_last_lr())
